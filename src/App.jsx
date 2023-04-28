1a6a04ac2da0a07d7a33702ac92f18b2548bd5bf
Zuber Padyar <juberpadyar111@gmail.com>
2023-04-28 11:41:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import "./App.css";
+import { blogList } from "./Components/Bloglist";
+import { motion } from "framer-motion";
+
+const imageAnimate={
+    offscreen:{x:-100, opacity:0},
+    onscreen:{
+    x:0,
+    opacity:1,
+    rotate:[360,360,0],
+    transition: {type:"spring",
+    bounce:0.4,
+    duration:1}
+  }
+
+}
+
+const textAnimate={
+    offscreen:{y:100, opacity:0},
+    onscreen:{y:0,
+    opacity:1,
+    transition: {type:"spring",
+    bounce:0.4,
+    duration:1}
+ }
+
+}
+
+function Card({ image, h2, p,id }) {
+  return (
+    <motion.div className="card " id={id}
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{once:false, amount:1}}
+        transition={{staggerChildren:0.6}}
+    >
+      <motion.div className="image-container"       
+        variants={imageAnimate}
+      >{image}</motion.div>
+      <motion.h2 
+        variants={textAnimate}
+      >{h2}</motion.h2>
+      <motion.p
+         variants={textAnimate}     
+      >{p}</motion.p>
+    </motion.div>
+  );
+}
+
+export default function App() {
+  return blogList.map((item, index) => (
+      <div className="card-wrapper" key={index}>
+          <Card image={item.image} h2={item.h2} p={item.p}  />
+      </div>
+  ));
+}
---
+++ src/Components/Bloglist.jsx
@@ -0,0 +1,103 @@
+export const blogList = [
+    {
+      image: "🍅",
+      h2: "Tomatoes",
+      p: "Tomatoes are a great source of vitamin C and potassium."
+    },
+    {
+      image: "🍊",
+      h2: "Oranges",
+      p: "Oranges are packed with vitamin C and fiber."
+    },
+    {
+      image: "🍋",
+      h2: "Lemons",
+      p: "Lemons are high in vitamin C and may aid in digestion."
+    },
+    {
+      image: "🍐",
+      h2: "Pears",
+      p: "Pears are a good source of fiber and vitamin C."
+    },
+    {
+      image: "🍏",
+      h2: "Apples",
+      p: "Apples are rich in antioxidants and fiber."
+    },
+    {
+      image: "🍇",
+      h2: "Grapes",
+      p: "Grapes are a good source of antioxidants and may reduce inflammation."
+    },
+    {
+      image: "🍆",
+      h2: "Eggplant",
+      p: "Eggplant is low in calories and high in fiber and antioxidants."
+    },
+    {
+      image: "🥦",
+      h2: "Broccoli",
+      p: "Broccoli is packed with vitamins, minerals, and fiber."
+    },
+    {
+      image: "🥕",
+      h2: "Carrots",
+      p: "Carrots are a great source of vitamin A and fiber."
+    },
+    {
+      image: "🍓",
+      h2: "Strawberries",
+      p: "Strawberries are high in vitamin C and antioxidants."
+    },
+    {
+      image: "🫐",
+      h2: "Blueberries",
+      p: "Blueberries are rich in antioxidants and may improve brain function."
+    },
+    {
+      image: "🥥",
+      h2: "Coconut",
+      p: "Coconut is high in healthy fats and may improve heart health."
+    },
+    {
+      image: "🍉",
+      h2: "Watermelon",
+      p: "Watermelon is mostly water and low in calories."
+    },
+    {
+      image: "🥭",
+      h2: "Mango",
+      p: "Mango is high in vitamin C and may boost immunity."
+    },
+    {
+      image: "🥑",
+      h2: "Avocado",
+      p: "Avocado is high in healthy fats and may improve heart health."
+    },
+    {
+      image: "🍍",
+      h2: "Pineapple",
+      p: "Pineapple is high in vitamin C and may aid digestion."
+    },
+    {
+      image: "🍑",
+      h2: "Peaches",
+      p: "Peaches are a good source of vitamin C and may reduce inflammation."
+    },
+    {
+      image: "🍒",
+      h2: "Cherries",
+      p: "Cherries are high in antioxidants and may reduce inflammation and pain."
+    },
+    {
+      image: "🍊",
+      h2: "Tangerines",
+      p: "Tangerines are high in vitamin C and may reduce stress."
+    },
+    {
+      image: "🥬",
+      h2: "Kale",
+      p: "Kale is packed with vitamins, minerals, and antioxidants."
+    }
+  ];
+  
